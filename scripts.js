56dccbae2d822deed6032a094e5f4ea4e073cf00
pedrobornio <pedrobornio@gmail.com>
2023-01-08 19:16:00 -0300

    update

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function clicou(event) {
 
   divContainer.innerHTML = `
   <iframe 
-  src="https://player.vimeo.com/video/787394992?h=8deefd0085&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+  src="https://player.vimeo.com/video/787394992?h=8deefd0085&amp;title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autoplay=1&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
   width="${largura}" 
   height="${altura}" 
   frameborder="0" 
