2b4b5004605e8e00fbe2b92b240b73b229bb868e
pedrobornio <pedrobornio@gmail.com>
2023-01-08 20:43:07 -0300

    update

--- scripts.js
+++ scripts.js
@@ -2,10 +2,11 @@ function clicou(event) {
   const divContainer = document.querySelector("#videoContainerCapa");
   var altura = document.querySelector("#videoContainerCapa").clientHeight;
   var largura = document.querySelector("#videoContainerCapa").clientWidth;
+  const divBotton = document.querySelector("#bottao");
 
   divContainer.innerHTML = `
   <iframe 
-  src="https://player.vimeo.com/video/787394992?h=8deefd0085&amp;title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autoplay=1&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+  src="https://player.vimeo.com/video/787412293?h=bba3688e52&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;autoplay=1&amp;app_id=58479" 
   width="${largura}" 
   height="${altura}" 
   frameborder="0" 
@@ -15,4 +16,11 @@ function clicou(event) {
   allowfullscreen 
   title="APERTE O PLAY"></iframe>
   `;
+
+  setTimeout(() => {
+    divBotton.innerHTML = `
+    <a href="https://pay.kiwify.com.br/dGF7c0o">
+    <div class="button">QUERO APRENDER AGORA!</div>
+    </a>`;
+  }, 360000);
 }
